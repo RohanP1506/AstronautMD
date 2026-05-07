7452e089999952177801b42a4e0888fa6b6d9565
koisoysauce <kai.jp2014@gmail.com>
2026-05-07 15:47:10 -0500

    Separate datasets into different files for cleaner execution

---
+++ observations/cytokine/immune_alamar.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+from cytokine_dataframe import *
+
+# Dataset straight from open-source
+immune_alamar = organize_df(pd.read_csv('https://osdr.nasa.gov/geode-py/ws/studies/OSD-575/download?source=datamanager&file=LSDS-8_Multiplex_serum.immune.AlamarPanel_TRANSFORMED.csv', index_col=0).transpose())
+
+# Clean up dataset
+immune_alamar_long = process_cytokine_df(immune_alamar, 'immune_alamar')
